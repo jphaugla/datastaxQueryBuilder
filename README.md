90b195407789e49c3eb42269eb2a0cd5a5b0a05b
Jason Haugland <jasonhaugland@gmail.com>
2019-08-28 15:53:34 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# datastaxQueryBuilder
-Examples using DataStax Cassandra query builder in 2.x DataStax driver
+# DataStax Query Builder	
+
+## About
+
+This github covers using DataStax Query Builder to do basic CRUD actions
+
+## Prerequisites
+
+* Basic understanding of Docker images and containers. 
+
+* Docker installed on your local system, see [Docker Installation Instructions](https://docs.docker.com/engine/installation/). 
+
+* When using Docker for Mac or Docker for Windows, the default resources allocated to the linux VM running docker are 2GB RAM and 2 CPU's. Make sure to adjust these resources to meet the resource requirements for the containers you will be running. More information can be found here on adjusting the resources allocated to docker.
+
+[Docker for mac](https://docs.docker.com/docker-for-mac/#advanced)
+
+[Docker for windows](https://docs.docker.com/docker-for-windows/#advanced)
+
+
+## Getting Started
+1. Prepare Docker environment-see the Prerequisites section above...
+2. Pull this github into a directory  
+```bash
+git clone https://github.com/jphaugla/datastaxQueryBuilder.git
+```
+3. Refer to the notes from DataStax Docker github for background on the needed DataStax images.  Directions are here:  [https://github.com/datastax/docker-images/#datastax-platform-overview](https://github.com/datastax/docker-images/#datastax-platform-overview).  Don't get too bogged down here as the included docker-compose.yaml handles most everything.
+4. Open terminal and change to the github home where you will see the docker-compose.yml file, then: 
+```bash
+docker-compose up -d
+```
+5. Verify DataStax is working (may take a minute for datastax cassandra to startup so be patient)
+```bash
+docker exec dse cqlsh -e "desc keyspaces"
+```
+6. At this point can use the command shell from cassandra using
+```bash
+docker exec -it dse cqlsh
+```
+## Conclusion
+At this point, run throught the cql worksheet and enjoy playing with Cassandra!
+
+##  Additional Notes/tips
+
+* To get the IP address of the DataStax and openldap:
+
+```bash
+export DSE_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dse)
+```
+   and:
+```bash
+export LDAP_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' openldap)
+```
+* use `echo $DSE_IPE` and `echo $LDAP_IP` to view
+* can add to the docker compose to run additional datastax workloads such as analtyics (-k), search (-s), and graph (-g)
+```bash
+ command:
+     -s
+     -g
+     -k
+```
---
+++ src/main/java/com/dse/demo/app/QBTest.java
@@ -0,0 +1,99 @@
+package com.dse.demo.app;
+
+import com.datastax.dse.driver.api.core.DseSession;
+import com.datastax.dse.driver.api.querybuilder.DseQueryBuilder;
+import com.datastax.oss.driver.api.core.ConsistencyLevel;
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.*;
+import com.datastax.oss.driver.api.querybuilder.BuildableQuery;
+import com.datastax.oss.driver.api.querybuilder.select.Select;
+
+import java.net.InetSocketAddress;
+
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.bindMarker;
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.selectFrom;
+
+public class QBTest {
+
+	public static void main(String[] args) {
+
+		String node = "jphmac";
+		String username = "cassandra";
+		String upass = "cassandra";
+
+		CqlSession session;
+
+		// Connect to the cluster and keyspace "authtest"
+		session = DseSession.builder().addContactPoint(new InetSocketAddress(node, 9042)).withLocalDatacenter("dc1").build();
+
+
+				// withAuthCredentials(username,upass).build();
+
+		// Create keyspace and table
+		session.execute("CREATE KEYSPACE IF NOT EXISTS authtest WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};");
+		session.execute("CREATE TABLE IF NOT EXISTS authtest.users (email text PRIMARY KEY, age int, city text, firstname text, lastname text);");
+
+		// Insert one record into the users table
+		session.execute("INSERT INTO authtest.users (lastname, age, city, email, firstname) VALUES ('Jones', 35, 'Austin', 'bob@example.com', 'Bob')");
+
+		// Select from table
+		ResultSet results = session.execute("SELECT * FROM authtest.users");
+		for (Row row : results) {
+			System.out.println("simple session execute");
+			System.out.format("%s %s %s %s %d\n", row.getString("firstname"), row.getString("lastname"), row.getString("city"), row.getString("email"), row.getInt("age"));
+		}
+		//  Build Query example
+
+		Select query = selectFrom("authtest", "users").column("email"); // SELECT email FROM authtest.users
+		SimpleStatement statement = query.build();
+
+		ResultSet rs = session.execute(statement);
+		Row row = rs.one();
+		System.out.println("Query Builder");
+		System.out.println(row.getString("email"));
+
+		// During application initialization with bindmarker
+
+		// Select selectUser = selectFrom("authtest","users").column("firstname").column("lastname").column("email").whereColumn("email").isEqualTo(bindMarker());
+		Select selectUser = selectFrom("authtest","users").all().whereColumn("email").isEqualTo(bindMarker());
+
+		// SELECT * FROM user WHERE id=?
+		PreparedStatement preparedSelectUser = session.prepare(selectUser.build());
+
+		// At runtime:
+		System.out.println("bound query builder");
+		ResultSet rs1 = session.execute(preparedSelectUser.bind("bob@example.com"));
+		Row row1 = rs1.one();
+		System.out.println(row1.getString("email") + " " + row1.getString("firstname") + " " + row1.getString("lastname"));
+			// INSERT INTO ks.users (id,first_name,last_name) VALUES (:id,:first_name,:last_name) USING TTL :ttl
+
+		BuildableQuery query1 = DseQueryBuilder.insertInto("authtest", "users")
+				.value("email", DseQueryBuilder.bindMarker("email"))
+				.value("firstname", DseQueryBuilder.bindMarker("firstname"))
+				.value("lastname", DseQueryBuilder.bindMarker("lastname"))
+				.value("city", DseQueryBuilder.bindMarker("city"))
+				.value("age",DseQueryBuilder.bindMarker("age"))
+				.usingTtl(DseQueryBuilder.bindMarker("ttl"));
+		PreparedStatement preparedStatement = session.prepare(query1.asCql());
+		BoundStatement boundStatement = preparedStatement.boundStatementBuilder()
+				.setString("email", "jasonhhh@gmail.com")
+				.setString("firstname", "Jason")
+				.setString("lastname", "Holison")
+				.setString("city", "Minneapolis")
+				.setInt("age",45)
+				.setConsistencyLevel(ConsistencyLevel.ALL)
+				.setInt("ttl", 3600)
+				.build();
+		session.execute(boundStatement);
+
+		//  final count all
+		Select query3 =
+				DseQueryBuilder.selectFrom("authtest", "users")
+						.countAll()
+						.whereColumn("email")
+						.isEqualTo(DseQueryBuilder.bindMarker("email"));
+		SimpleStatement statement1 = query3.build();
+		ResultSet rs3 = session.execute(statement1);
+
+	}
+}
